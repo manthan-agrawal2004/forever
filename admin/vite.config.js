e7cdad4e71117e47eb9f68bd22a3fd7333929cd9
Manthan Agrawal <agmanthan2004@gmail.com>
2024-12-13 22:18:10 +0530

    commmit_for_deployment

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{port:5174}
-})
+  optimizeDeps: {
+    include: ['react-router-dom'], // Add this line
+  },
+  build: {
+    rollupOptions: {
+      external: ['react-router-dom'], // Add this line if you intend to externalize it
+    },
+  },
+});
